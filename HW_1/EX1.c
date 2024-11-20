d7ff3c1b661b00a6dd331e344cbd2888ebd1d244
ItamarMeir <itamarmeir1@gmail.com>
2024-11-20 09:25:35 +0200

    Implement process generation printing in print_pids function

--- HW_1/EX1.c
+++ HW_1/EX1.c
@@ -31,6 +31,30 @@ pid_t my_fork(void){
 }
 
 /**
+ * For each g=0, 1, ..., G, the function should print N_g lines in the following format:
+ * My pid is p. My generation is g.
+ * 
+ * Where:
+ *  - g is the generation of the process: the generation of the original (first) process is 0.
+ *    If the generation of a process is i, then the generation of its child process is i+1.
+ *  - p is the pid of the process which prints the line.
+ * 
+ * For instance, the line may be: My pid is 1038. My generation is 0.
+ * 
+ * Note that the pids are arbitrary, and are dependent upon the processes’ scheduling on your machine.
+ * In particular, the operating system may allocate a new process the same pid of an old terminated process.
+ * The function should print its output to the file, whose file descriptor is the input fd.
+ * 
+ * Limitations and hints:
+ *  - The function should use a single loop.
+ *  - Each process iterates exactly N times over the loop.
+ *  - Each process prints a single line.
+ *  - Use my_fork(), not fork().
+ *  - No prints nor calls to my_fork() are allowed outside the loop.
+ *  - Each process should print its line only after all its children printed their lines.
+ */
+
+/**
  * Function: print_pids
  * --------------------
  * Prints the process IDs and their generations to the specified file descriptor.
@@ -41,29 +65,27 @@ pid_t my_fork(void){
  *  G  - maximum generation of processes
  */
 void print_pids(int fd, short unsigned int N, short unsigned int G) {
-    int generation = 0;
+    int generation = 0;                 // Initialize generation variable
     for (int i = 0; i < N; i++) {
         if (generation < G) {
             pid_t my_pid = my_fork();
             if (my_pid == 0) {
                 // Child process
-              generation++;
-              i = -1;
-              continue;
+              generation++; // Increment generation
+              i = -1;    // Reset loop counter
+              continue; // Restart loop
             }
-            else wait(NULL);
+            else wait(NULL);    // Parent process - wait for child process to finish
         }    
-                
-        
     }
-    dprintf(fd, "My pid is %d. My generation is %d.\n", getpid(), generation);
-    exit(0); // Exit child process       
+    dprintf(fd, "My pid is %d. My generation is %d.\n", getpid(), generation);  // Print process ID and generation
+    exit(0); // Exit child process 
 }
 
 int main(void) {
     int fd = STDOUT_FILENO; // File descriptor (using standard output for this example)
     short unsigned int N = 2; // Number of iterations each process should perform
-    short unsigned int G = 3; // Maximum generation of processes
+    short unsigned int G = 2; // Maximum generation of processes
 
     print_pids(fd, N, G);
 
